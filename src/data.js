ccae970b352a1ca42da01504a3f7176c21a78a86
AnaBahena <ana-bahena@live.com.mx>
2019-07-02 14:36:21 -0500

    Ya se puede pintar el nombre de los campeones

--- src/data.js
+++ src/data.js
@@ -1,10 +1,30 @@
 window.data ={
 
+  championSearch: (data) => {
+    let cardGeneral = [];
+    for(let i in data ){
+      let name = data[i]["name"];
+      let title = data[i]["title"];
+      let info = data[i]["info"];
+      let tags = data[i]["tags"];
+
+       cardGeneral.push(
+         {
+           name,
+           title,
+           info,
+           tags
+          }
+          );
+
+
+    }
   
-},
+    return (cardGeneral);
+} 
+};
+
 window.data=data;
-
-
 
 // Esta función permite obtener el nombre de todos los campeones
 // championSearch: (data,id,title) => {
@@ -18,3 +38,4 @@ window.data=data;
 //   }
 // }
 // }
+
--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
-// let sectionPrueba= document.getElementById ("prueba").innerHTML=window.LOL();
+// let cardGeneral= document.getElementById("cardGeneral");
+// cardGeneral.insertAdjacentHTML("afterbegin",  `<section id="prueba"></section>`);
+
+const prueba = document.getElementById('prueba')
+
+
+const result = window.data.championSearch(LOL.data);
+
+result.forEach(element => {
+
+prueba.insertAdjacentHTML("beforeend",  `<p id="prueba">${element.name}</p>`);
+
+    
+});
+
+
 
