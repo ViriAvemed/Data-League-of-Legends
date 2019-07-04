b4c97a0e47f61f53aa2aa953ec4463928c392a0c
Avellaneda <vir>
2019-07-04 09:38:18 -0500

    Subiendo cambios de disenio

--- src/data.js
+++ src/data.js
@@ -26,35 +26,9 @@ window.data ={
   
     return (cardGeneral);
 } 
-};
+},
 
 
-Object.filter = function( obj, predicate) {
-  var result = {}, key;
-  // ---------------^---- as noted by @CMS, 
-  //      always declare variables with the "var" keyword
-
-  for (key in obj) {
-      if (obj.hasOwnProperty(key) && !predicate(obj[key])) {
-          result[key] = obj[key];
-      }
-  }
-
-  return result;
-};
 
 window.data=data;
-
-// Esta función permite obtener el nombre de todos los campeones
-// championSearch: (data,id,title) => {
-// const lolData=data;
-// let championsId = Object.keys (lolData);
-// // console.log (championsId)
-// for (let i=0; i<championsId.length; i++){
-//   if (championsId[i].id===id){
-//     result=championsId[i]
-//     console.log (result);
-//   }
-// }
-// }
 
