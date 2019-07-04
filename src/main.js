8bfd38d8e779f2523d31c95c4b1ba883e3797e8c
Avellaneda <vir>
2019-07-04 12:32:41 -0500

    Se limpia la informacion del css

--- src/main.js
+++ src/main.js
@@ -43,6 +43,7 @@ function volverpintar () {
 // console.log(orderAZ);
 
 const boton = document.getElementById("eliminar");
+
 function borrar (){
   document.getElementById("prueba").innerHTML=""
 }
@@ -58,34 +59,5 @@ boton2.addEventListener("click",volverpintar);
 
 
 
-//
-// result.forEach(element => {
-//     card=
-//     `<div id="cardGen" class="cardGen">
-//     <div id="cardChampion" class="cardChampion">
-//     <div id= "card-text" class="card-text">
-//         <p>${element.name}</p>
-//         <p>${element.title}</p>
-//         <p>${element.tags}</p>
-//     </div>
-//     <tr id="card-imagen class="card-imagen" align="middle"  ><img src="${element.splash}" width="100%" height="auto" </tr>
-//     <div id "card-atributos" class="card-atributos">
-//         <p>attack ${element.info.attack}</p>
-//         <p>defense ${element.info.defense}</p>
-//         <p>magic ${element.info.magic}</p>
-//         <p>difficulty ${element.info.difficulty}</p>
-//         <p>movespeed ${element.stats.movespeed}</p>
-//         <p>attackdamage ${element.stats.attackdamage}</p>
-//         <p>attackspeedoffset ${element.stats.attackspeedoffset}</p>
-//         <p>hp ${element.stats.hp}</p>
-//         <p>hp regen ${element.stats.hpregen}</p>
-//         <p>hp per level ${element.stats.hpperlevel}</p>
-//         <p>mp ${element.stats.mp}</p>
-//
-//     </div>
-//     </div>
-//     </div>`
-//     prueba.insertAdjacentHTML("beforeend",card);
-// });
-//
+
 //
