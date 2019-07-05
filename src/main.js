79417fb395fcaf0e1f9e1ca24067dcae99222e9b
AnaBahena <ana-bahena@live.com.mx>
2019-07-05 08:52:49 -0500

    Se le ha dado estilo a los botones y se ha cambiado el tipo de letra del encabezado

--- src/main.js
+++ src/main.js
@@ -5,25 +5,15 @@
 const prueba = document.getElementById('prueba');
 const boton2 = document.getElementById("seaForName");
 const boton = document.getElementById("eliminar");
-
 const result = window.data.championSearch(LOL.data);
-const boton = document.getElementById("eliminar");
-const boton2 = document.getElementById("seaForName");
 const change1 = document.getElementById('champFilter');
 const change2 = document.getElementById('filterRol');
 const change3 = document.getElementById('filterRol2');
 
 
-<<<<<<< HEAD
-//Función para puntar todas las tarjetas con las propiedades seleccionadas en el data.js
-let volverpintar = () => {
-  result.forEach(element => {
-      
-=======
 
 const volverpintar =(results) => {
   results.forEach(element => {
->>>>>>> 5e238b478ffdc5377e1ed95443653a621195d3a9
     card =`<div class="flip-card">
     <div class="flip-card-inner">
     <div class="flip-card-front">
@@ -45,8 +35,103 @@ const volverpintar =(results) => {
 
     prueba.insertAdjacentHTML("beforeend", card);
   });
-<<<<<<< HEAD
+  return volverpintar;
+};
+
+
+
+//Función para borrar tajetas de todos los campeones
+let borrar = () =>{
+  document.getElementById("prueba").innerHTML=""
 }
+
+
+
+const orden = () =>{
+    let val=change3.value;
+    let newArray;
+  
+    borrar();
+    const result1 = window.data.championSearch(LOL.data);
+    if(val==='D'){
+      newArray=result1.reverse()
+      volverpintar(newArray);
+    }else if(val==='A'){
+      newArray=result1;
+      volverpintar(newArray);
+    }
+  
+  };
+
+
+  const buscarNombre = () =>{
+
+
+    let valorabuscar = document.getElementById('buscName').value;
+    let newArray = result.filter(function (el) {
+      return el.name.toLowerCase().indexOf(valorabuscar) !== -1;
+    });
+    borrar();
+    volverpintar(newArray);
+  
+  };
+
+
+  const rol = () =>{
+    let valorabuscar = document.getElementById('filterRol').value;
+    let newArray = result.filter(function (el) {
+      return el.tags.indexOf(valorabuscar) !== -1;
+  
+    });
+    borrar();
+    volverpintar(newArray);
+  
+  };
+
+
+
+//Detonar botón para puntar todas las tarjetas
+boton2.addEventListener("click",volverpintar);
+//Detonar botón para borrar tarjetas
+boton.addEventListener("click",borrar);
+boton2.addEventListener("click",buscarNombre);
+// change1.addEventListener("change",mejoresCampeones);
+change2.addEventListener("change",rol);
+change3.addEventListener("change",orden);
+
+
+
+
+
+//Función para filtrar campeones por mayor ataque
+
+let attack=document.getElementById("champFilter").value;
+let printattack=() =>{
+    let newarray = result.filter (result => result.info.attack ==="10");
+    volverpintar(newarray);
+// console.log(printattack);
+borrar();
+volverpintar(newArray);
+};
+
+
+attack.addEventListener("change",printattack);
+
+
+
+
+//Función para filtrar campeones por mayor dificultad
+let difficulty = result.filter (result => result.info.difficulty ===10);
+// console.log (difficulty);
+
+//Función para filtrar campeones por mayor defensa
+let defense = result.filter(result => result.info.defense >=9);
+// console.log (defense);
+
+//Función para filtrar campeones por mayor magia
+let magic = result.filter(result => result.info.magic ===10);
+// console.log (magic);
+
 
 
 // //Función para ordenar de la A a la Z
@@ -58,111 +143,3 @@ const volverpintar =(results) => {
 // return -1
 // });
 // // console.log(orderAZ);
-
-
-
-//Función para borrar tajetas de todos los campeones
-let borrar = () =>{
-  document.getElementById("prueba").innerHTML=""
-}
-
-//Detonar botón para borrar tarjetas
-boton.addEventListener("click",borrar);
-=======
-  return volverpintar;
-};
-
-
-const borrar = () =>{
-  document.getElementById("prueba").innerHTML=""
-};
->>>>>>> 5e238b478ffdc5377e1ed95443653a621195d3a9
-
-//Detonar botón para puntar todas las tarjetas
-boton2.addEventListener("click",volverpintar);
-
-
-// let searchName = () =>{
-// result.filter (result => result.name ===document.getElementById ("buscName").value)
-// };
-
-// document.getElementById("buscName");
-
-// buscName.addEventListener("change", searchName);
-
-
-//Función para filtrar campeones por mayor ataque
-
-let attack=document.getElementById("ataque").value;
-let printattack=() =>{
-    let newarray = result.filter (result => result.info.attack ==="10");
-    volverpintar(newarray);
-// console.log(printattack);
-
-};
-borrar();
-attack.addEventListener("change",printattack);
-
-
-<<<<<<< HEAD
-=======
-const orden = () =>{
-  let val=change3.value;
-  let newArray;
->>>>>>> 5e238b478ffdc5377e1ed95443653a621195d3a9
-
-  borrar();
-  const result1 = window.data.championSearch(LOL.data);
-  if(val==='D'){
-    newArray=result1.reverse()
-    volverpintar(newArray);
-  }else if(val==='A'){
-    newArray=result1;
-    volverpintar(newArray);
-  }
-
-};
-
-const buscarNombre = () =>{
-
-<<<<<<< HEAD
-//Función para filtrar campeones por mayor defensa
-let defense = result.filter(result => result.info.defense >=9);
-// console.log (defense);
-=======
-  let valorabuscar = document.getElementById('buscName').value;
-  let newArray = result.filter(function (el) {
-    return el.name.toLowerCase().indexOf(valorabuscar) !== -1;
-  });
-  borrar();
-  volverpintar(newArray);
-
-};
->>>>>>> 5e238b478ffdc5377e1ed95443653a621195d3a9
-
-const rol = () =>{
-  let valorabuscar = document.getElementById('filterRol').value;
-  let newArray = result.filter(function (el) {
-    return el.tags.indexOf(valorabuscar) !== -1;
-
-<<<<<<< HEAD
-//Función para filtrar campeones por mayor magia
-let magic = result.filter(result => result.info.magic ===10);
-// console.log (magic);
-=======
-  });
-  borrar();
-  volverpintar(newArray);
->>>>>>> 5e238b478ffdc5377e1ed95443653a621195d3a9
-
-};
-
-boton.addEventListener("click",borrar);
-boton2.addEventListener("click",buscarNombre);
-// change1.addEventListener("change",mejoresCampeones);
-change2.addEventListener("change",rol);
-change3.addEventListener("change",orden);
-
-//Función para filtrar campeones por mayor dificultad
-let difficulty = result.filter (result => result.info.difficulty ===10);
-// console.log (difficulty);
