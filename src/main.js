3e23db507e8461c6e9e4a6e427ce2cdbf42b1607
ViridianaAvem <viridiana.avem@gmail.com>
2019-11-14 01:41:47 -0600

    modificando background body

--- src/main.js
+++ src/main.js
@@ -60,6 +60,25 @@ const prom1 = ()=> {
     `;
 };
 
+//slider
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  const x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
 
 
 
