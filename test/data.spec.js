288b0436598c6af3b951d3653a2429c5fcbaffa9
ViridianaAvem <viridiana.avem@gmail.com>
2019-11-13 23:39:13 -0600

    agregando diseño responsivo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,60 +1,18 @@
-require('../src/data.js');
-
-// const newarrayData = window.data.debugData;
-
-const arrayDebug = window.data.arrayDebug;
+require('../src/data.js'); 
+require('../src/data/lol/lol.js'); 
 
 
-//Test de funcion
-describe('debugData', ()=>{
-  it('Deberìa ser una función', () =>{
-    expect(typeof debugData).toBe('function');
+describe('ord', () => { 
+  it('is a function', () => { 
+      expect(typeof ord).toBe('function'); 
   });
+
+  // it('returns `Aatrox`', () => { //segunda sentencia
+  //     expect(window.filterData('name', 'Aatrox', window.lol.results)[0].name).toBe('Aatrox'); //Llama la función de filterData. Tiene 3 parametros(key, value, data)
+  // });
+
+  // it('returns `There are 44 Mortys`', () => {
+  //     expect(window.ord('name', 'Aatrox', window.lol.results).length).toBe(1);
+  // });
 });
-
-//Test que ordena la data de forma ascendente y descendente
-const filterbyletter = window.data.filterbyletter;
-const filterByBestChampions = window.data.filterByBestChampions;
-
-const input = [{name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']},
-  {name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage']},
-  {name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']},
-  {name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank']}
-
-];
-
-
-const output2 = [
-  {name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']},
-  {name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage']},
-  {name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']},
-  {name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank']}
-];
-
-
-describe('filterbyletter', ()=>{
-  it('Deberìa ser una función', () =>{
-    expect(typeof filterbyletter).toBe('function');
-  });
-
-  it('deberia retornar de forma ascendente, de A-Z', () => {
-    expect(filterbyletter("A", input)).toEqual(output2);
-  });
-  it('deberia retornar d de forma descendente, de Z-A', () => {
-    expect(filterbyletter("Z", output2 )).toEqual(output2);
-  });
-
-  });
-
-
-// describe('filterByBestChampions', ()=>{
-//   it('Deberìa ser una función', () =>{
-//     expect(typeof filterByBestChampions).toBe('function');
-//
-//   });
-//   it('debería retornar a los mejores en ataque', () =>{
-//
-//     expect(filterByBestChampions('attack',input).toBe(input));
-//   });
-// });
 
